13067e096392867140aa4efbb38af5197139036d
unknown <duypq2610@gmail.com>
2018-09-17 14:14:19 +0700

    write sweptAABB done

--- CastleVaniaSource/CastleVaniaSource/SweptAABB.h
+++ CastleVaniaSource/CastleVaniaSource/SweptAABB.h
@@ -2,13 +2,11 @@
 #define _SWEPT_AABB_H
 
 #include "Global.h"
-#include <math.h>
-#include <algorithm>
-#include <limits>
 
-extern bool AABBCheck(Box object, Box other);
-extern bool AABB(Box object, Box other, float& moveX, float& moveY);
-extern Box GetSweptBroadphaseBox(Box obj);
-extern int Collide(Box &box, Box &block, float& collisionTime, float& x, float& y);
+extern bool isColliding(Box object, Box other);
+extern bool isAABB(Box object, Box other, float& moveX, float& moveY);
+extern Box getSweptBroadphaseBox(Box obj);
+extern int Collide(Box &object, Box &other, float& collisionTime, float& x, float& y);
 extern float SweptAABB(Box object, Box other, float& normalX, float& normalY);
+
 #endif
